f2b04996f3747b1849ab98ae9b90b90e147734eb
kwrl <haakongk@stud.ntnu.no>
2015-02-06 19:52:16 +0100

    Removed shitty stuff

--- src/RemoteTic.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-
-import javax.swing.event.ListSelectionEvent;
-
-public interface RemoteTic extends Remote {
-	void setClient(ClientTicTacToe client);
-	void valueChanged(ListSelectionEvent evt);
-}
